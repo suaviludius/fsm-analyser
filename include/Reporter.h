e5efbe0e8aa60416dad0e0ad35c73616a4ff7807
Artem <powerranger1912@gmail.com>
2026-05-30 16:48:48 +0300

    style: не люблю я эти строковые сдвиги от кланга

--- include/Reporter.h
+++ include/Reporter.h
@@ -9,8 +9,7 @@ namespace fsm {
 class Reporter {
 public:
     static std::string generateCsv(const std::vector<Anomaly>& anomalies);
-    static void saveToFile(const std::vector<Anomaly>& anomalies,
-                          const std::string& filename);
+    static void saveToFile(const std::vector<Anomaly>& anomalies, const std::string& filename);
 
 private:
     static std::string formatDuration(std::chrono::milliseconds duration);
